4c0012a2014f3a1d58e693715d324e68cd40a8f3
MiataRacer28 <littl28@rocketmail.com>
2025-03-31 14:46:02 -0400

    Moved Views and URLs to base app
    Moved views and URLs to base app. Updated urlpatterns in NinerExchange folder to detect base.

--- NinerExchange/NinerExchange/urls.py
+++ NinerExchange/NinerExchange/urls.py
@@ -1,12 +1,11 @@
 
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path, include
 
-def home(request): #Home Page View
-    return HttpResponse('Home Page') 
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', include('base.urls')),
+    
 ]
